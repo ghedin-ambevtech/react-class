6d86e9b7379494e9a8cdc6c8df063436d692b2af
Alison Ghedin <alison.ghedin@ambevtech.com.br>
2021-02-15 06:13:27 -0300

    estado da aplicacao

---
+++ react_basico/04_estado_aplicacao.md
@@ -0,0 +1,11 @@
+# Estado da aplicação
+
+- Propriedades não podem ser alteradas, são somente leitura. Para trabalhar dessa forma, alteramos nossa função do componente para uma classe.
+- Transformando o app em uma classe podemos adicionar o estado(state = {}) de uma aplicação. Classe não tem retorno então retira () de App.
+- A classe extende Component. Deverá ser importado Component.
+- A classe terá uma função render(), que retorna seu componente.
+- Existe uma nova forma de trabalhar que não seja com state, trata-se de hooks.
+- quando usa uma lista do state, é necessário que tenha uma chave unica.
+
+- Quando componentes só possuem propriedades, chamamos de stateless componentes. Caso contrários, stateful components(componentes inteligentes).
+  -Nossa aplicação inteira só tem um estado!
--- react_basico/src/App.js
+++ react_basico/src/App.js
@@ -1,22 +1,47 @@
+import { Component } from 'react';
 import './App.css';
 import Comentario from './components/Comentario'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu projeto</h1>
-      <Comentario nome="João" email="joao@gmail.com" data={new Date(2021, 2, 15)}>
-        Olá, tudo bem?
+class App extends Component {
+  
+  state = {
+    comentarios: [
+      {
+        nome: 'João',
+        email: 'joao@gmail.com',
+        data: new Date(2021, 2, 15),
+        mensagem: 'Olá, tudo bem?'
+      },
+      {
+        nome: 'Maria',
+        email: 'maria@gmail.com',
+        data: new Date(2021, 2, 14),
+        mensagem: 'Outra mensagem'
+      },
+      {
+        nome: 'José',
+        email: 'jose@gmail.com',
+        data: new Date(2021, 2, 15),
+        mensagem: 'Sem comentários'
+      }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Meu projeto</h1>
+        {this.state.comentarios.map((comentario, indice) => (
+        <Comentario
+        key={indice} 
+        nome={comentario.nome}
+        email={comentario.email}
+        data={comentario.data}>
+        {comentario.mensagem}
       </Comentario>  
-      <Comentario nome="Maria" email="maria@gmail.com" data={new Date(2021, 2, 15)}>
-      Trocando mensagem
-      </Comentario>  
-      <Comentario nome="José" email="josé@gmail.com" data={new Date(2021, 2, 14)}>
-      Olá, tudo bem?
-      </Comentario>  
-
-    </div>
-  );
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
